b3ade7a7f286b9f2bb224bf85afcde7807df1f31
María José Briones <mbrionesr@fen.uchile.cl>
2026-02-27 19:10:40 +0100

    clean ddl and pokemon update

--- all_ddl.sql
+++ all_ddl.sql
@@ -3,7 +3,7 @@ CREATE TABLE users( --1-N to payments; 1-N to subscriptions; 1-N to rides
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   email_id VARCHAR(100) UNIQUE NOT NULL,
-  dob DATE NOT NULL, --what's this variable?
+ -- dob DATE NOT NULL, --what's this variable?
   reg_date DATE --registered date.
 );
 
@@ -15,11 +15,11 @@ CREATE TABLE stations( --1-N to bikes
 
 CREATE TABLE bikes( --1-N to rides;  N-1 to stations; 1-N to maintenances
   bike_id INT PRIMARY KEY,
-  bike_type VARCHAR(20) NOT NULL,
+  bike_type TEXT CHECK (bike_type IN ('manual', 'electric')) NOT NULL,
   bike_status TEXT CHECK (bike_status IN ('docked','in use','under maintenance')), --status seemed to be a function, also shared with status on maintenances
   current_station_id INT, --N-1 to stations
   battery_level FLOAT,
-  purchase_date DATE,
+  -- purchase_date DATE,
   CHECK (battery_level>=20),
   CONSTRAINT fk_current_station_id FOREIGN KEY (current_station_id) REFERENCES stations(station_id) --definition of FK 
 );
@@ -51,7 +51,8 @@ CREATE TABLE maintenances(
 
 CREATE TABLE subscriptions(
   sub_id INT PRIMARY KEY,
-  user_id INT, --N-1 to users
+  user_id INT, --N-1 to users,
+  cost NUMERIC (6,2) NOT NULL, 
   sub_type TEXT CHECK (sub_type IN ('monthly','yearly','quarterly')),
   begining_date DATE NOT NULL, --start_date seems to be a function? 
   end_date DATE,
@@ -67,14 +68,3 @@ CREATE TABLE payments(
   payment_status TEXT CHECK (payment_status IN ('success','failed')),
   FOREIGN KEY (user_id) REFERENCES users(user_id) --definition of FK
 );
-
--- why are we using update functions (DML) on a DLL?
-ALTER table bikes
-ALTER column bike_type TYPE text;
-
-ALTER TABLE bikes
-  ALTER COLUMN bike_type SET NOT NULL;
-
-ALTER TABLE bikes
-  ADD CONSTRAINT bikes_bike_type_check
-  CHECK (bike_type IN ('manual', 'electric'));
